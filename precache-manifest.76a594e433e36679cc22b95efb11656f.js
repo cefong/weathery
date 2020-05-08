8d95d8d245db14666cfdf098c9e007578764dbbf
Celine Fong <celinefong4@gmail.com>
2020-05-08 14:53:54 -0600

    Updates

---
+++ precache-manifest.76a594e433e36679cc22b95efb11656f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fd774fd18b2d8776c86e86bc8b4b666",
+    "url": "/weatherpanel/index.html"
+  },
+  {
+    "revision": "ebd8db2c587154e1a363",
+    "url": "/weatherpanel/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "0de955650366b6eeb4c5",
+    "url": "/weatherpanel/static/css/main.5aff7a03.chunk.css"
+  },
+  {
+    "revision": "ebd8db2c587154e1a363",
+    "url": "/weatherpanel/static/js/2.8703d3b7.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/weatherpanel/static/js/2.8703d3b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0de955650366b6eeb4c5",
+    "url": "/weatherpanel/static/js/main.d1e2dff4.chunk.js"
+  },
+  {
+    "revision": "bb580aed6e9513351aae",
+    "url": "/weatherpanel/static/js/runtime-main.148a4277.js"
+  },
+  {
+    "revision": "ee40b9143707ec2f0b3923a060c76810",
+    "url": "/weatherpanel/static/media/BlondeFont.ee40b914.ttf"
+  },
+  {
+    "revision": "51461db43ab7362a8c5f3dedc920e05a",
+    "url": "/weatherpanel/static/media/Cloud.51461db4.svg"
+  },
+  {
+    "revision": "57afbca9df8e5ca1da0e0dc41e090d64",
+    "url": "/weatherpanel/static/media/Fog.57afbca9.svg"
+  },
+  {
+    "revision": "2cf3bb1c2e51770601dbc9acf4da75ac",
+    "url": "/weatherpanel/static/media/Rain.2cf3bb1c.svg"
+  },
+  {
+    "revision": "85b6d51b9d624f1b9e91148da470c473",
+    "url": "/weatherpanel/static/media/Snow.85b6d51b.svg"
+  },
+  {
+    "revision": "a922c3fdad02e0b1cba8aaf702b7d24c",
+    "url": "/weatherpanel/static/media/Storm.a922c3fd.svg"
+  },
+  {
+    "revision": "18ae8b6a5ffd4e0f90940179de8a3b80",
+    "url": "/weatherpanel/static/media/Sun.18ae8b6a.svg"
+  },
+  {
+    "revision": "e977faa6c2d9b406aa98ab80eb66e552",
+    "url": "/weatherpanel/static/media/Wind.e977faa6.svg"
+  }
+]);
